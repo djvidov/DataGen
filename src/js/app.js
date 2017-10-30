4e8d175c2e95493669cfb59fb5a61ff5192cf3c3
Ovidiu <djvidov@yahoo.com>
2017-10-30 23:18:47 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ function generareCnp() {
     document.getElementById("generare").innerText = cnp;
 }
 
-function generareCNP(sexString, an, luna, zi, judet) {
+function generareCNP(sexString, anFull, luna, zi, judet) {
     var currentSex;
     if(sexString == "masculin"){
         currentSex = SexMasculin;
@@ -23,18 +23,18 @@ function generareCNP(sexString, an, luna, zi, judet) {
         currentSex = SexFeminin;
     }
     var sex;
-    if (an >= 1800 && an <= 1899) {
+    if (anFull >= 1800 && anFull <= 1899) {
         sex = Between1800And1899 + currentSex;
     }
-    else if ( an >= 1900 && an <= 1999 ) {
+    else if ( anFull >= 1900 && anFull <= 1999 ) {
         sex = Between1900And1999 + currentSex;
     }   
     else {
         sex = Between2000And2099 + currentSex;
     }
     var cifre = Math.floor(Math.random() * 999) + 001;
-    var an1 = an.substring(2);
-    var cnp = sex + an1 + luna + zi + judet + cifre;
+    var anShort = anFull.substring(2);
+    var cnp = sex + anShort + luna + zi + judet + cifre;
     var control = (cnp.substr(0,1) * 2 + cnp.substr(1,1) * 7 + cnp.substr(2,1) * 9 + 
         cnp.substr(3,1) * 1 + cnp.substr(4,1) * 4 + cnp.substr(5,1) * 6 + 
         cnp.substr(6,1) * 3 + cnp.substr(7,1) * 5 + cnp.substr(8,1) * 8 + 
