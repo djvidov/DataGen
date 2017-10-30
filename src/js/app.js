4c1dbaa5bd6ec2a888e45d7a62be9a8c0e23b3fd
Ovidiu <djvidov@yahoo.com>
2017-10-30 23:09:03 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,42 @@
-function generareCnp() {
-    var s = document.getElementById("sex").value;
-    var an = document.getElementById("an").value;
-    if(s == "masculin" ){
-        if (an >= 1800 && an <= 1899) {
-            sex = 3;
-        }
-        else if ( an >= 1900 && an <= 1999 ) {
-            sex = 1;
-        }   
-        else {
-            sex = 5;
-        }
-    }
+var SexMasculin = 1;
+var SexFeminin = 2;
+var Between1800And1899 = 2;
+var Between1900And1999 = 0;
+var Between2000And2099 = 4;
 
-    if(s == "feminin") {
-        if( an >= 1800 && an <= 1899) {
-            sex = 4;
-        } 
-        else if( an >= 1900 && an <= 1999) {
-            sex = 2;
-        }
-        else {
-            sex = 6;
-        }
-    }    
-       
-    var cifre = Math.floor(Math.random() * 999) + 001;
-    var an1 = an.substring(2);
+function generareCnp() {
+    var sex = document.getElementById("sex").value;
+    var an = document.getElementById("an").value;
     var luna = document.getElementById("luna").value;
     var zi = document.getElementById("zi").value;
-    var jud = document.getElementById("judet").value;
-    var cnp = sex + an1 + luna + zi + jud + cifre;
+    var judet = document.getElementById("judet").value;
+    var cnp = generareCNP(sex, an, luna, zi, judet);
+    document.getElementById("generare").innerText = cnp;
+}
+
+function generareCNP(sexString, an, luna, zi, judet) {
+    var currentSex;
+    if(sexString == "masculin"){
+        currentSex = SexMasculin;
+    }
+    else if(sexString == "feminin"){
+        currentSex = SexFeminin;
+    }
+
+    var sex;
+    if (an >= 1800 && an <= 1899) {
+        sex = Between1800And1899 + currentSex;
+    }
+    else if ( an >= 1900 && an <= 1999 ) {
+        sex = Between1900And1999 + currentSex;
+    }   
+    else {
+        sex = Between2000And2099 + currentSex;
+    }
+
+    var cifre = Math.floor(Math.random() * 999) + 001;
+    var an1 = an.substring(2);
+    var cnp = sex + an1 + luna + zi + judet + cifre;
 
     var control = (cnp.substr(0,1) * 2 + cnp.substr(1,1) * 7 + cnp.substr(2,1) * 9 + 
         cnp.substr(3,1) * 1 + cnp.substr(4,1) * 4 + cnp.substr(5,1) * 6 + 
@@ -42,5 +48,5 @@ function generareCnp() {
     } 
     cnp = cnp + control;
     
-    document.getElementById("generare").innerText = cnp;
+    return cnp;
 }
