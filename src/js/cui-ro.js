1d7484d1bcc07bc802dc0313f36e383943a47add
Marius M <marius_mym@yahoo.com>
2017-11-01 18:44:58 +0200

    modificare CUI js

--- src/js/cui-ro.js
+++ src/js/cui-ro.js
@@ -1,6 +1,10 @@
+
 function generateCIF(){
-    var tva = document.getElementById('tva').value;
-    var CIFtoDisplay = document.getElementById('newCIF')
+    var CIFfull = CIFtva(generateCIFdata(CIFlength()));
+    document.getElementById('newCIF').innerText = CIFfull; 
+}
+
+function CIFlength(){
     var CIF6n = Math.floor (Math.random() * 900000) + 100000;
     var CIF7n = Math.floor (Math.random() * 9000000) + 1000000;
     var CIF8n = Math.floor (Math.random() * 90000000) + 10000000;
@@ -19,16 +23,19 @@ function generateCIF(){
     else {
         chosenCIF = CIF9n;
     }
-    
-    var k = chosenCIF.toString();
+    return chosenCIF;
+}
 
-    if (chosenCIF == CIF6n){
+function generateCIFdata(chosenCIF){
+    var k = chosenCIF.toString();
+    var CIFlength = k.length;
+    if (CIFlength == 6 ){
         var keyNr = parseInt(k.charAt(5))*2+parseInt(k.charAt(4))*3+parseInt(k.charAt(3))*5+parseInt(k.charAt(2))*7+parseInt(k.charAt(1))*1+parseInt(k.charAt(0))*2;
     }
-    else if (chosenCIF == CIF7n){
+    else if (CIFlength == 7){
         var keyNr = parseInt(k.charAt(6))*2+parseInt(k.charAt(5))*3+parseInt(k.charAt(4))*5+parseInt(k.charAt(3))*7+parseInt(k.charAt(2))*1+parseInt(k.charAt(1))*2+parseInt(k.charAt(0))*3;
     }
-    else if (chosenCIF == CIF8n){
+    else if (CIFlength == 8){
         var keyNr = parseInt(k.charAt(7))*2+parseInt(k.charAt(6))*3+parseInt(k.charAt(5))*5+parseInt(k.charAt(4))*7+parseInt(k.charAt(3))*1+parseInt(k.charAt(2))*2+parseInt(k.charAt(1))*3+parseInt(k.charAt(0))*5;    
     }
     else {
@@ -41,10 +48,15 @@ function generateCIF(){
 
     var chosenCIFk = chosenCIF.toString() + keyNr.toString();
 
-    if (tva == 'Da'){
-        CIFtoDisplay.innerText = 'RO'+chosenCIFk;
+    return chosenCIFk;
+}
+
+function CIFtva (chosenCIFk){
+    var tva = document.getElementById('tva').value;
+    if (tva == "Da"){
+        return "RO"+chosenCIFk
     }
-    else{
-        CIFtoDisplay.innerText = chosenCIFk;
+    else {
+        return chosenCIFk
     }
 }
