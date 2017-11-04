adc1d827168583efc8ccd41785cde39ed60ad9ba
mariusmym <marius_mym@yahoo.com>
2017-11-04 15:42:04 +0200

    adaugare domeniu pentru email

--- src/js/email.js
+++ src/js/email.js
@@ -1,6 +1,6 @@
 // [random-part] - un set de 5-10 caractere generate random cu o functie a-z,0-9,_
 
-function showEmail(){ ///pt test
+function showEmail(){ 
     var email = randomPart();
     document.getElementById('newEmail').innerText = email; 
 }
@@ -10,6 +10,18 @@ function randomPart(length, character) {
     character = '0123456789_abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     var result = '';
     for (var i = length; i > 0; --i) result += character[Math.floor(Math.random() * character.length)];
+    if (result.length == 6){
+        result += '@gmail.com'
+    }
+    else if(result.length == 7){
+        result += '@yahoo.com'
+    }
+    else if(result.length == 9){
+        result += '@outlook.com'
+    }
+    else{
+        result += '@office.com'
+    }
     return result;
 }
 
