3eceff26b12abc429754ce965f0d654b6d9ce67a
Marius <marius_mym@yahoo.com>
2017-11-09 15:29:33 +0200

    functie "copy" pentru numar de telefon

--- src/js/app.js
+++ src/js/app.js
@@ -123,63 +123,95 @@ function showNumber(){
 }
 
 // NUMAR TELEFON -------------------------------------------------------------------//
+function showPhF(){
+    var PhFix = generateFIX();
+    document.getElementById('newPhF').value = PhFix;
+};
+
 function generateFIX(){
     var oper = document.getElementById("operator").value;
     var jud = document.getElementById("jud").value;
-    var phoneFixToDisplay = document.getElementById("newPhF")
+    var phoneFixToDisplay = "";
 
     var rndm6 = Math.floor (Math.random() * 900000) + 100000;
     var rndm7 = Math.floor (Math.random() * 9000000) + 1000000; //pt fix bucuresti
 
     if (oper == "vdf") {
-        phoneFixToDisplay.innerText = "0372"+ rndm6
+        phoneFixToDisplay = "0372"+ rndm6
     } 
     else if(oper == "org") {
-        phoneFixToDisplay.innerText = "0374"+ rndm6
+        phoneFixToDisplay = "0374"+ rndm6
     } 
     else if (oper == "romtl" && jud == "alba") {
-        phoneFixToDisplay.innerText = "0258"+ rndm6
+        phoneFixToDisplay = "0258"+ rndm6
     } 
     else if (oper == "digi" && jud == "alba") {
-        phoneFixToDisplay.innerText = "0358"+ rndm6
+        phoneFixToDisplay = "0358"+ rndm6
     } 
     else if (oper == "romtl" && jud == "arad") {
-        phoneFixToDisplay.innerText = "0257"+ rndm6
+        phoneFixToDisplay = "0257"+ rndm6
     } 
     else if (oper == "digi" && jud == "arad") {
-        phoneFixToDisplay.innerText = "0357"+ rndm6
+        phoneFixToDisplay = "0357"+ rndm6
     }
     else if (oper == "romtl" && jud == "arges") {
-        phoneFixToDisplay.innerText = "0248"+ rndm6
+        phoneFixToDisplay = "0248"+ rndm6
     } 
     else if (oper == "digi" && jud == "arges") {
-        phoneFixToDisplay.innerText = "0348"+ rndm6
+        phoneFixToDisplay = "0348"+ rndm6
     }
+    return phoneFixToDisplay;
     //continuare cu celelalte judete
 }
 
+function CopyPhF() {
+    document.getElementById("newPhF").select();
+    document.execCommand('copy');
+    var element = document.getElementById("copyPhF");
+    element.innerHTML= "✔";
+    setTimeout(function(){
+        element.innerHTML = "copiaza numar";
+    },250);
+};
+
+function showMobil(){
+    var PhMob = generateMobil();
+    document.getElementById('newPhM').value = PhMob;
+};
+
 function generateMobil(){
     var operM = document.getElementById("operatorM").value;
-    var phoneMobileToDisplay = document.getElementById("newPhM");
+    var phoneMobileToDisplay = "";
 
     var rndm7 = Math.floor (Math.random() * 9000000) + 1000000; 
     var choseMobPref = Math.random(); // pt alegerea aleatorie a cifrei de dupa "07..." pt operatorii mobili cu cate doua intrevale (072x -073x vdf) (074x - 075x orange)
     if (operM == "vdfM" && choseMobPref < 0.495){
-        phoneMobileToDisplay.innerText = "072"+ rndm7
+        phoneMobileToDisplay = "072"+ rndm7
     }
     else if(operM == "vdfM" && choseMobPref >= 0.495){
-        phoneMobileToDisplay.innerText = "073"+ rndm7
+        phoneMobileToDisplay = "073"+ rndm7
     }
     else if (operM == "orgM" && choseMobPref < 0.495){
-        phoneMobileToDisplay.innerText = "074"+ rndm7
+        phoneMobileToDisplay = "074"+ rndm7
     }
     else if(operM == "orgM" && choseMobPref >= 0.495){
-        phoneMobileToDisplay.innerText = "075"+ rndm7
+        phoneMobileToDisplay = "075"+ rndm7
     }
     else if(operM == "tlk"){
-        phoneMobileToDisplay.innerText = "076"+ rndm7
+        phoneMobileToDisplay = "076"+ rndm7
     }
     else{
-        phoneMobileToDisplay.innerText = "077"+ rndm7   
+        phoneMobileToDisplay = "077"+ rndm7   
     }
+    return phoneMobileToDisplay;
 }
+
+function CopyPhM() {
+    document.getElementById("newPhM").select();
+    document.execCommand('copy');
+    var element = document.getElementById("copyPhM");
+    element.innerHTML= "✔";
+    setTimeout(function(){
+        element.innerHTML = "copiaza numar";
+    },250);
+};
