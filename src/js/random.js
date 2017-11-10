8c4483aec43bca98a25924dd477000a5367441b7
Marius <marius_mym@yahoo.com>
2017-11-10 11:27:48 +0200

    codacy issues

--- src/js/random.js
+++ src/js/random.js
@@ -1,12 +1,12 @@
 function getRandom() {
-    var minStr = document.getElementById('minValue').value;
+    var minStr = document.getElementById("minValue").value;
     var min = parseInt(minStr);
-    var maxStr = document.getElementById('maxValue').value;
+    var maxStr = document.getElementById("maxValue").value;
     var max = parseInt(maxStr);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function showNumber(){
     var random = getRandom();
-    document.getElementById('newRandom').value = random;
+    document.getElementById("newRandom").value = random;
 }
