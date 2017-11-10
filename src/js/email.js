d432b8763887d82b4403f75040c232d670e0a0b5
Marius <marius_mym@yahoo.com>
2017-11-10 11:41:58 +0200

    codacy email

--- src/js/email.js
+++ src/js/email.js
@@ -1,30 +1,4 @@
 // [random-part] - un set de 5-10 caractere generate random cu o functie a-z,0-9,_
-
-function showEmail(){ 
-    var email = generateEmail();
-    document.getElementById('newEmail').value = email; 
-}
-
-function generateEmail() {
-    var length = chooseLength();
-    let character = '0123456789abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    var result = '';
-    for (var i = length; i > 0; i--) result += character[Math.floor(Math.random() * character.length)];
-    if (result.length == 6) {
-        result += '@gmail.com';
-    }
-    else if(result.length == 7){
-        result += '@yahoo.com';
-    }
-    else if(result.length == 9){
-        result += '@outlook.com';
-    }
-    else{
-        result += '@office.com';
-    }
-    return result;
-}
-
 function chooseLength(){
     var strlength = Math.random().toString().substr(2, 1);
     var length = parseInt(strlength);
@@ -34,19 +8,34 @@ function chooseLength(){
     return length;
 }
 
-function copyEmail() {
-    document.getElementById("newEmail").select();
-    document.execCommand('copy');
-    var element = document.getElementById('check');
-    element.setAttribute("style","display:block;");
-    setTimeout(function(){
-        element.setAttribute("style","display:none;");
-    },250);
-};
+function generateEmail() {
+    var length = chooseLength();
+    let character = "0123456789abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var result = "";
+    for (var i = length; i > 0; i--) result += character[Math.floor(Math.random() * character.length)];
+    if (result.length == 6) {
+        result += "@gmail.com";
+    }
+    else if(result.length == 7){
+        result += "@yahoo.com";
+    }
+    else if(result.length == 9){
+        result += "@outlook.com";
+    }
+    else{
+        result += "@office.com";
+    }
+    return result;
+}
+
+function showEmail(){ 
+    var email = generateEmail();
+    document.getElementById("newEmail").value = email; 
+}
 
 function copyEmail() {
     document.getElementById("newEmail").select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     var element = document.getElementById("copyEmail");
     element.innerHTML= "✔";
     setTimeout(function(){
