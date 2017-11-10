dadec732e4260b6778e376a007427ad9ddbb0071
Marius <marius_mym@yahoo.com>
2017-11-10 11:26:19 +0200

    fixed codacy issues

--- src/js/tel-ro.js
+++ src/js/tel-ro.js
@@ -1,9 +1,4 @@
 
-function showPhF(){
-    var PhFix = generateFIX();
-    document.getElementById('newPhF').value = PhFix;
-};
-
 function generateFIX(){
     var oper = document.getElementById("operator").value;
     var jud = document.getElementById("jud").value;
@@ -12,48 +7,50 @@ function generateFIX(){
     var rndm6 = Math.floor (Math.random() * 900000) + 100000;
     var rndm7 = Math.floor (Math.random() * 9000000) + 1000000; //pt fix bucuresti
 
-    if (oper == "vdf") {
-        phoneFixToDisplay = "0372"+ rndm6
+    if (oper === "vdf") {
+        phoneFixToDisplay = "0372"+ rndm6;
     } 
-    else if(oper == "org") {
-        phoneFixToDisplay = "0374"+ rndm6
+    else if(oper === "org") {
+        phoneFixToDisplay = "0374"+ rndm6;
     } 
-    else if (oper == "romtl" && jud == "alba") {
-        phoneFixToDisplay = "0258"+ rndm6
+    else if (oper === "romtl" && jud === "alba") {
+        phoneFixToDisplay = "0258"+ rndm6;
     } 
-    else if (oper == "digi" && jud == "alba") {
-        phoneFixToDisplay = "0358"+ rndm6
+    else if (oper === "digi" && jud === "alba") {
+        phoneFixToDisplay = "0358"+ rndm6;
     } 
-    else if (oper == "romtl" && jud == "arad") {
-        phoneFixToDisplay = "0257"+ rndm6
+    else if (oper === "romtl" && jud === "arad") {
+        phoneFixToDisplay = "0257"+ rndm6;
     } 
-    else if (oper == "digi" && jud == "arad") {
-        phoneFixToDisplay = "0357"+ rndm6
+    else if (oper === "digi" && jud === "arad") {
+        phoneFixToDisplay = "0357"+ rndm6;
     }
-    else if (oper == "romtl" && jud == "arges") {
-        phoneFixToDisplay = "0248"+ rndm6
+    else if (oper === "romtl" && jud === "arges") {
+        phoneFixToDisplay = "0248"+ rndm6;
     } 
-    else if (oper == "digi" && jud == "arges") {
-        phoneFixToDisplay = "0348"+ rndm6
+    else if (oper === "digi" && jud === "arges") {
+        phoneFixToDisplay = "0348"+ rndm6;
     }
     return phoneFixToDisplay;
     //continuare cu celelalte judete
 }
 
+function showPhF(){
+    var PhFix = generateFIX();
+    document.getElementById("newPhF").value = PhFix;
+}
+
+
 function copyPhF() {
     document.getElementById("newPhF").select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     var element = document.getElementById("copyPhF");
     element.innerHTML= "✔";
     setTimeout(function(){
         element.innerHTML = "copiaza numar";
     },250);
-};
+}
 
-function showMobil(){
-    var PhMob = generateMobil();
-    document.getElementById('newPhM').value = PhMob;
-};
 
 function generateMobil(){
     var operM = document.getElementById("operatorM").value;
@@ -61,25 +58,30 @@ function generateMobil(){
 
     var rndm7 = Math.floor (Math.random() * 9000000) + 1000000; 
     var choseMobPref = Math.random(); // pt alegerea aleatorie a cifrei de dupa "07..." pt operatorii mobili cu cate doua intrevale (072x -073x vdf) (074x - 075x orange)
-    if (operM == "vdfM" && choseMobPref < 0.495){
-        phoneMobileToDisplay = "072"+ rndm7
+    if (operM === "vdfM" && choseMobPref < 0.495){
+        phoneMobileToDisplay = "072"+ rndm7;
     }
-    else if(operM == "vdfM" && choseMobPref >= 0.495){
-        phoneMobileToDisplay = "073"+ rndm7
+    else if(operM === "vdfM" && choseMobPref >= 0.495){
+        phoneMobileToDisplay = "073"+ rndm7;
     }
-    else if (operM == "orgM" && choseMobPref < 0.495){
-        phoneMobileToDisplay = "074"+ rndm7
+    else if (operM === "orgM" && choseMobPref < 0.495){
+        phoneMobileToDisplay = "074"+ rndm7;
     }
-    else if(operM == "orgM" && choseMobPref >= 0.495){
-        phoneMobileToDisplay = "075"+ rndm7
+    else if(operM === "orgM" && choseMobPref >= 0.495){
+        phoneMobileToDisplay = "075"+ rndm7;
     }
-    else if(operM == "tlk"){
-        phoneMobileToDisplay = "076"+ rndm7
+    else if(operM === "tlk"){
+        phoneMobileToDisplay = "076"+ rndm7;
     }
     else{
-        phoneMobileToDisplay = "077"+ rndm7   
+        phoneMobileToDisplay = "077"+ rndm7;  
     }
     return phoneMobileToDisplay;
+}
+
+function showMobil(){
+    var PhMob = generateMobil();
+    document.getElementById("newPhM").value = PhMob;
 }
 
 function copyPhM() {
@@ -90,4 +92,4 @@ function copyPhM() {
     setTimeout(function(){
         element.innerHTML = "copiaza numar";
     },250);
-};
+}
