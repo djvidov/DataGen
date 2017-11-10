dd3c05cb0b4a0e9014417e8a87b9e83ddd026b1e
Marius <marius_mym@yahoo.com>
2017-11-10 12:22:09 +0200

    codacy email

--- src/js/email.js
+++ src/js/email.js
@@ -2,7 +2,7 @@
 function chooseLength(){
     var strlength = Math.random().toString().substr(2, 1);
     var length = parseInt(strlength);
-    if (length == 0 || length == 1 || length == 2 || length == 3){
+    if (length === 0 || length === 1 || length === 2 || length === 3){
         length = 6
     }
     return length;
@@ -12,14 +12,16 @@ function generateEmail() {
     var length = chooseLength();
     let character = "0123456789abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var result = "";
-    for (var i = length; i > 0; i--) result += character[Math.floor(Math.random() * character.length)];
-    if (result.length == 6) {
+    for (var i = length; i > 0; i--){
+        result += character[Math.floor(Math.random() * character.length)]
+    }
+    if (result.length === 6) {
         result += "@gmail.com";
     }
-    else if(result.length == 7){
+    else if(result.length === 7){
         result += "@yahoo.com";
     }
-    else if(result.length == 9){
+    else if(result.length === 9){
         result += "@outlook.com";
     }
     else{
@@ -41,4 +43,4 @@ function copyEmail() {
     setTimeout(function(){
         element.innerHTML = "copiaza adresa";
     },250);
-};
+}
