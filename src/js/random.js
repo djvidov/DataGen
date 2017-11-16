b42ba1650e80f7b209e8c02caefb4563aacbb9af
Marius <marius_mym@yahoo.com>
2017-11-16 11:22:50 +0200

    separare functie generare de functia legata de HTML

--- src/js/random.js
+++ src/js/random.js
@@ -1,12 +1,13 @@
-function getRandom() {
-    var minStr = document.getElementById("minValue").value;
-    var min = parseInt(minStr);
-    var maxStr = document.getElementById("maxValue").value;
-    var max = parseInt(maxStr);
+
+function getRandom(minVal, maxVal) {
+    var min = parseInt(minVal);
+    var max = parseInt(maxVal);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function showNumber(){
-    var random = getRandom();
+    var minVal = document.getElementById("minValue").value;
+    var maxVal = document.getElementById("maxValue").value;
+    var random = getRandom(minVal, maxVal);
     document.getElementById("newRandom").value = random;
 }
