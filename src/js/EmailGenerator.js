77475f5a7ea6abf0353d07a9cd3f04ef8889c381
Marius <marius_mym@yahoo.com>
2017-11-16 10:50:00 +0200

    Refactoring Email function (prototyping)

---
+++ src/js/EmailGenerator.js
@@ -0,0 +1,40 @@
+
+function EmailGenerator (){
+
+    this.chooseLength = function(){
+    var strlength = Math.random().toString().substr(2, 1);
+    var length = parseInt(strlength);
+    if (length === 0 || length === 1 || length === 2 || length === 3){
+        length = 6;
+    }
+    return length;
+    };
+
+    this.generateEmail = function(length) {
+        let character = "0123456789abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var result = "";
+        for (var i = length; i > 0; i --){
+            result += character[Math.floor(Math.random() * character.length)];
+        }
+        if (result.length === 6) {
+            result += "@gmail.com";
+        }
+        else if(result.length === 7){
+            result += "@yahoo.com";
+        }
+        else if(result.length === 9){
+            result += "@outlook.com";
+        }
+        else{
+            result += "@office.com";
+        }
+        return result;
+    };
+
+    this.generate = function() {
+        var chooseLength = this.chooseLength();
+        var generateEmail = this.generateEmail(chooseLength);
+        return generateEmail;
+    };
+
+}
--- src/js/email.js
+++ src/js/email.js
@@ -1,38 +1,8 @@
-// [random-part] - un set de 5-10 caractere generate random cu o functie a-z,0-9,_
-function chooseLength(){
-    var strlength = Math.random().toString().substr(2, 1);
-    var length = parseInt(strlength);
-    if (length === 0 || length === 1 || length === 2 || length === 3){
-        length = 6;
-    }
-    return length;
-}
-
-function generateEmail() {
-    var length = chooseLength();
-    let character = "0123456789abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var result = "";
-    for (var i = length; i > 0; i--){
-        result += character[Math.floor(Math.random() * character.length)];
-    }
-    if (result.length === 6) {
-        result += "@gmail.com";
-    }
-    else if(result.length === 7){
-        result += "@yahoo.com";
-    }
-    else if(result.length === 9){
-        result += "@outlook.com";
-    }
-    else{
-        result += "@office.com";
-    }
-    return result;
-}
 
 function showEmail(){ 
-    var email = generateEmail();
-    document.getElementById("newEmail").value = email; 
+    var email = new EmailGenerator();
+    var newEmail = email.generate();
+    document.getElementById("newEmail").value = newEmail; 
 }
 
 function copyEmail() {
