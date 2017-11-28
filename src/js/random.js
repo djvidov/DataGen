a3b9396ee2f9b8564035fecc2b47d51ad5048502
Marius M <marius_mym@yahoo.com>
2017-11-28 19:01:44 +0200

    Random cu doua zecimale

--- src/js/random.js
+++ src/js/random.js
@@ -1,9 +1,19 @@
 
 function getRandom(minVal, maxVal) {
+    var getInt = document.getElementById("zecimale").value;
+    if (getInt === "Nu"){
     var min = parseInt(minVal);
     var max = parseInt(maxVal);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    else{
+        var min = parseInt(minVal);
+        var max = parseInt(maxVal);
+        return (Math.round(Math.random() * 100) / 100 * (max - min ) + min).toFixed(2);
+    }
+
 }
+
 
 function showNumber(){
     var minVal = document.getElementById("minValue").value;
@@ -11,3 +21,4 @@ function showNumber(){
     var random = getRandom(minVal, maxVal);
     document.getElementById("newRandom").value = random;
 }
+
