76876dd9e9d6eaaf0614a3ea90881cfecd563696
Marius <marius_mym@yahoo.com>
2017-12-07 10:16:43 +0200

    Random - impvovements in code style

--- src/js/random.js
+++ src/js/random.js
@@ -1,26 +1,23 @@
 
-function getRandom(minVal, maxVal) {
-    var zecimalsStr = document.getElementById("zecimalsNr").value;
+function getRandom(minVal, maxVal ,zecimalsStr) {
     var zecimalsInt = parseInt(zecimalsStr);
-    var getInt = document.getElementById("hasZecimals").value;
-    if (getInt === "Nu"){
     var min = parseInt(minVal);
     var max = parseInt(maxVal);
+    var getInt = document.getElementById("hasZecimals").value;
+    if (getInt === "Nu"){
     return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     else{
-        var min = parseInt(minVal);
-        var max = parseInt(maxVal);
         return (Math.random() * (max - min ) + min).toFixed(zecimalsInt);
     }
-
 }
 
 
 function showNumber(){
     var minVal = document.getElementById("minValue").value;
     var maxVal = document.getElementById("maxValue").value;
-    var random = getRandom(minVal, maxVal);
+    var zecimalsStr = document.getElementById("zecimalsNr").value;
+    var random = getRandom(minVal, maxVal ,zecimalsStr);
     document.getElementById("newRandom").value = random;
 }
 
