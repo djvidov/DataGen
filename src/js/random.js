7c909c54356614c5e4f41361787e2180d4556a06
Marius <marius_mym@yahoo.com>
2017-12-11 16:51:00 +0200

    Random - Codacy issues

--- src/js/random.js
+++ src/js/random.js
@@ -3,21 +3,12 @@ function getRandom(minVal, maxVal, zecimalsStr, zecimals) {
     var zecimalsInt = parseInt(zecimalsStr);
     var min = parseInt(minVal);
     var max = parseInt(maxVal);
-    if (zecimals == false){
+    if (zecimals === false){
     return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     else{
         return (Math.random() * (max - min ) + min).toFixed(zecimalsInt);
     }
-}
-
-function showNumber(){
-    var minVal = document.getElementById("minValue").value;
-    var maxVal = document.getElementById("maxValue").value;
-    var zecimalsStr = document.getElementById("zecimalsNr").value;
-    var zecimals = hasZecimals();
-    var random = getRandom(minVal, maxVal ,zecimalsStr, zecimals);
-    document.getElementById("newRandom").value = random;
 }
 
 function hasZecimals(){
@@ -28,4 +19,13 @@ function hasZecimals(){
     else {
         return false;
     }
+}
+
+function showNumber(){
+    var minVal = document.getElementById("minValue").value;
+    var maxVal = document.getElementById("maxValue").value;
+    var zecimalsStr = document.getElementById("zecimalsNr").value;
+    var zecimals = hasZecimals();
+    var random = getRandom(minVal, maxVal ,zecimalsStr, zecimals);
+    document.getElementById("newRandom").value = random;
 }
