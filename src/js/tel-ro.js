241fa2acd1e137d4bb3c941e2c02f8cd74fcb9a3
djvidov <djvidov@yahoo.com>
2018-01-01 18:30:50 +0200

    renaming

--- src/js/tel-ro.js
+++ src/js/tel-ro.js
@@ -1,70 +1,72 @@
 
-function generateFIX(){
-    var oper = document.getElementById("operator").value;
-    var jud = document.getElementById("jud").value;
-    var digiPrefixFix = "03"
-    var romtlcPrefixFix = "02"
-    var phoneFixToDisplay = "";
+function generateFIX(fixOperator, judet){
+    var digiPrefixFix = "03";
+    var telekomPrefixFix = "02";
+    var vodaphonePrefixFix = "0372";
+    var orangePrefixFix = "0374";
+    var phoneFix = "";
 
     var rndm6 = Math.floor (Math.random() * 900000) + 100000;
     var rndm7 = Math.floor (Math.random() * 9000000) + 1000000; //pt fix bucuresti
 
-    if (oper === "vdf") {
-        phoneFixToDisplay = "0372"+ rndm6;
+    if (fixOperator === "vdf") {
+        phoneFix = vodaphonePrefixFix + rndm6;
     } 
-    else if(oper === "org") {
-        phoneFixToDisplay = "0374"+ rndm6;
+    else if(fixOperator === "org") {
+        phoneFix = orangePrefixFix + rndm6;
     } 
-    else if (oper === "romtl" && jud === "1") {
-        phoneFixToDisplay = romtlcPrefixFix + jud + rndm7;
+    else if (fixOperator === "romtl" && judet === "1") {
+        phoneFix = telekomPrefixFix + judet + rndm7;
     } 
-    else if (oper === "digi" && jud === "1") {
-        phoneFixToDisplay = digiPrefixFix + jud + rndm7;
+    else if (fixOperator === "digi" && judet === "1") {
+        phoneFix = digiPrefixFix + judet + rndm7;
     } 
-    else if (oper === "romtl") {
-        phoneFixToDisplay = romtlcPrefixFix + jud + rndm6;
+    else if (fixOperator === "romtl") {
+        phoneFix = telekomPrefixFix + judet + rndm6;
     } 
-    else if (oper === "digi") {
-        phoneFixToDisplay = digiPrefixFix + jud + rndm6;
+    else if (fixOperator === "digi") {
+        phoneFix = digiPrefixFix + judet + rndm6;
     } 
-    return phoneFixToDisplay;
+    return phoneFix;
 }
 
 function showPhF(){
-    var PhFix = generateFIX();
+    var fixOperator = document.getElementById("operator").value;
+    var judet = document.getElementById("jud").value;
+    var PhFix = generateFIX(fixOperator, judet);
     document.getElementById("newPhF").value = PhFix;
 }
 
-function generateMobil(){
-    var operM = document.getElementById("operatorM").value;
-    var phoneMobileToDisplay = "";
+function generateMobil(mobileOperator){
+    var phoneMobile = "";
 
-    var rndm7 = Math.floor (Math.random() * 9000000) + 1000000; 
+    var randomPart = Math.floor (Math.random() * 9000000) + 1000000; 
     var choseMobPref = Math.random(); // pt alegerea aleatorie a cifrei de dupa "07..." pt operatorii mobili cu cate doua intrevale (072x -073x vdf) (074x - 075x orange)
-    if (operM === "vdfM" && choseMobPref < 0.495){
-        phoneMobileToDisplay = "072"+ rndm7;
+    if (mobileOperator === "vdfM" && choseMobPref < 0.495){
+        phoneMobile = "072"+ randomPart;
     }
-    else if(operM === "vdfM" && choseMobPref >= 0.495){
-        phoneMobileToDisplay = "073"+ rndm7;
+    else if(mobileOperator === "vdfM" && choseMobPref >= 0.495){
+        phoneMobile = "073"+ randomPart;
     }
-    else if (operM === "orgM" && choseMobPref < 0.495){
-        phoneMobileToDisplay = "074"+ rndm7;
+    else if (mobileOperator === "orgM" && choseMobPref < 0.495){
+        phoneMobile = "074"+ randomPart;
     }
-    else if(operM === "orgM" && choseMobPref >= 0.495){
-        phoneMobileToDisplay = "075"+ rndm7;
+    else if(mobileOperator === "orgM" && choseMobPref >= 0.495){
+        phoneMobile = "075"+ randomPart;
     }
-    else if(operM === "tlk"){
-        phoneMobileToDisplay = "076"+ rndm7; 
+    else if(mobileOperator === "tlk"){
+        phoneMobile = "076"+ randomPart; 
     }
     else{
-        phoneMobileToDisplay = "077"+ rndm7;  // digi 
+        phoneMobile = "077"+ randomPart;  // digi 
     }
-    return phoneMobileToDisplay;
+    return phoneMobile;
 }
 
 function showMobil(){
-    var PhMob = generateMobil();
-    document.getElementById("newPhM").value = PhMob;
+    var mobileOperator = document.getElementById("operatorM").value;
+    var phoneNumber = generateMobil(mobileOperator);
+    document.getElementById("newPhM").value = phoneNumber;
 }
 
 function copyFunction(newValue, copyValue) {
