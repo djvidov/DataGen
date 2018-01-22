2d987a9ffef9053667d227c3d800bbfb41a4e554
Marius <marius_mym@yahoo.com>
2018-01-22 15:33:28 +0200

    verify tel - schimbari de functionalitate

--- src/js/verifyTel.js
+++ src/js/verifyTel.js
@@ -14,31 +14,34 @@ function verifyPhoneNumber(phoneNumberStr){
     var noWithout00 = removeDouble0(phoneNumberStr);
     var ISDcodes = [93, 355, 213, 1684, 376, 244, 1264, 672, 1268, 54, 374, 297, 61, 43, 994, 1242, 973, 880, 1246, 375, 32, 501, 229, 1441, 975, 591, 387, 267, 55, 246, 1284, 673, 359, 226, 257, 855, 237, 1, 238, 1345, 236, 235, 56, 86, 61, 61, 57, 269, 682, 506, 385, 53, 599, 357, 420, 243, 45, 253, 1767, 1809, 1829, 1849, 670, 593, 20, 503, 240, 291, 372, 251, 500, 298, 679, 358, 33, 689, 241, 220, 995, 49, 233, 350, 30, 299, 1473, 1671, 502, 441481, 224, 245, 592, 509, 504, 852, 36, 354, 91, 62, 98, 964, 353, 441624, 972, 39, 225, 1876, 81, 441534, 962, 7, 254, 686, 383, 965, 996, 856, 371, 961, 266, 231, 218, 423, 370, 352, 853, 389, 261, 265, 60, 960, 223, 356, 692, 222, 230, 262, 52, 691, 373, 377, 976, 382, 1664, 212, 258, 95, 264, 674, 977, 31, 599, 687, 64, 505, 227, 234, 683, 850, 1670, 47, 968, 92, 680, 970, 507, 675, 595, 51, 63, 64, 48, 351, 1787, 1939, 974, 242, 262, 40, 7, 250, 590, 290, 1869, 1758, 590, 508, 1784, 685, 378, 239, 966, 221, 381, 248, 232, 65, 1721, 421, 386, 677, 252, 27, 82, 211, 34, 94, 249, 597, 47, 268, 46, 41, 963, 886, 992, 255, 66, 228, 690, 676, 1868, 216, 90, 993, 1649, 688, 1340, 256, 380, 971, 44, 1, 598, 998, 678, 379, 58, 84, 681, 212, 967, 260, 263];
     var justNumber = noWithout00.match(/[0-9]+/g).join(""); //numar cu prefix international fara + si fara () . -  etc
-    var totalNumbers = justNumber.length;
-    var extractedISDcode1 = justNumber.substring(0, totalNumbers - 10); // cod ISD cu un caracter
-    var extractedISDcode2 = justNumber.substring(0, totalNumbers - 9); //// cod ISD cu 2 caractere
+    var totalDigits = justNumber.length;
+    var extractedISDcode1 = justNumber.substring(0, totalDigits - 10); // numere din 12 caractere cu prefix international
+    var extractedISDcode2 = justNumber.substring(0, totalDigits - 9); //// numere din 11 caractere cu prefix international
     var last10Digits = justNumber.slice(-10); // pentru alte verificari viitoare in functie de tara, gen potrivire cu un format anume, 0x.. , 07xx, verificarea prefixelor operatorilor etc
     var getRouPrefix = last10Digits.substr(0, 4); //extragere prefix de Romania
     var rouMobilePrefix = /(07[2-6]\d|077[0-4]|0788|0799)/g; //regex pentru prefixele RO
 
-    if (totalNumbers >= 8 && totalNumbers <= 16) { //majoritatea numerelor de telefon au intre 8 si 16 caractere (cu prefix international inclus)
+    if (totalDigits >= 8 && totalDigits <= 16) { //majoritatea numerelor de telefon au intre 8 si 16 caractere (cu prefix international inclus)
         if (ISDcodes.includes(parseInt(extractedISDcode1)) || ISDcodes.includes(parseInt(extractedISDcode2)) ) { // verifica pt cod international
-            if(getRouPrefix.match(rouMobilePrefix)){ //pt Romania
-                return "valid 1";
+            if(getRouPrefix.match(rouMobilePrefix) && totalDigits === 11){ //pt Romania
+                return "valid RO cu prefix int.";
+            }
+            else if (ISDcodes.includes(parseInt(extractedISDcode1)) && extractedISDcode1 != 40){
+                return "posibil valid (numar strain)";
             }
             else{
-                return "invalid 1";
+                return "prefix RO dar numar invalid";
             }  
         } 
         else if (getRouPrefix.match(rouMobilePrefix)){ // verificare prefixe Romania ... ( ar mai trebui o conditie ??? )
-            return "valid 2";
+            return "valid RO fara prefix int";
         }
         else {
-            return "invalid 2";
+            return "invalid 2 - prefix int incorect";
         }
     }
     else{
-        return "format incor.";
+        return "format incorect";
     }
 }
 
