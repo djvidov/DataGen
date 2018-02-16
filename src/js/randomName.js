598accc6c4b4ac6efd8372cb5b9cb274668b3885
Marius <marius_mym@yahoo.com>
2018-02-16 11:34:39 +0200

    random Name - problem SOLVED !!!

--- src/js/randomName.js
+++ src/js/randomName.js
@@ -1,33 +1,49 @@
 
-var nume = null;
-nume = ["Popescu", "Niță", "Pop", "Popa", "Ionescu", "Nemeș", "Stan", "Dumitrescu", "Dima", "Gheorghiu", "Ioniță","Marin","Tudor", 
-"Dobre", "Barbu", "Nistor", "Florea", "Ene", "Dinu", "Georgescu", "Stoica", "Diaconu", "Diaconescu", "Mazilu","Andrei", "Marinescu", 
-"Nistor", "Mocanu", "Oprea", "Voinea", "Dochioiu", "Albu", "Tabacu", "Manole", "Cristea", "Toma", "Stănescu", "Preda", "Pușcașu", "Tomescu"];
-nume["en"] = [""];
-nume["fr"] = [""];
+nume = {
+    ro: ["Popescu", "Niță", "Pop", "Popa", "Ionescu", "Nemeș", "Stan", "Dumitrescu", "Dima", "Gheorghiu", "Ioniță","Marin","Tudor", 
+    "Dobre", "Barbu", "Nistor", "Florea", "Ene", "Dinu", "Georgescu", "Stoica", "Diaconu", "Diaconescu", "Mazilu","Andrei", "Marinescu", 
+    "Nistor", "Mocanu", "Oprea", "Voinea", "Dochioiu", "Albu", "Tabacu", "Manole", "Cristea", "Toma", "Stănescu", "Preda", "Pușcașu", "Tomescu"],
 
-var prenume = null;
-prenume = ["Ana", "Alexandru", "Mihaela", "Andreea", "Elena", "Adrian", "Andrei", "Alexandra", "Mihai", "Ionut", "Cristina", "Florin", "Daniel", "Cristina-Maria",
-"Marian", "Marius", "Cristian", "Daniela", "Alina", "Maria", "Daniel-Georgian", "Marian-Andrei", "Marius-Andrei", "Ioana", "Constantin", "Nicoleta", 
-"Georgiana", "Mariana", "Bogdan", "Vasile", "Gabriel", "Gabriela", "Nicolae", "Gheorghe", "George", "Ioan", "Valentin", "Adriana", "Ionela", "Catalin", 
-"Stefan", "Ion", "Florentina", "Anca-Iulia", "Anamaria", "Simona", "Iulian", "Roxana", "Oana", "Irina", "Cristian-Vasile", "Daniela-Elena", "Florin-Andrei",
-"Alina-Livia", "Maria-Elena", "Vasile-Ion", "Gabriel-Ionut", "Diana", "Mirela", "Iuliana", "Madalina", "Raluca", "Ionel", "Lucian", "Cosmin", "Sorin", 
-"Loredana", "Claudia", "Monica", "Ramona-Maria", "George-Andrei", "Ana", "Ciprian", "Corina", "Laura", "Vlad Razvan", "Radu", "Liliana", "Valentina", 
-"Viorel", "Iulia", "Claudia-Elena", "Monica-Larisa", "Ramona", "Ana-Ioana", "Ciprian-Andrei", "Corina-Maria", "Laura-Cristina", "Vlad-Alexandru", "Razvan-Andrei", 
-"Radu-George", "Liliana-Elena", "Valentina-Andreea", "Viorel-Marin", "Iulia-Ioana", "Andrei-Marius", "Alexandra-Maria", "Mihai-Cristian", "Ionut-Marius"];
+    en: ["Clarkson", "Hamill", "Ridley", "Stevard", "Lewis", "White", "Brown", "Taylor", "Evans"],
 
-prenume["en"] = ["Ana", "Alexandru", "Mihaela", "Andreea", "Elena", "Adrian"];
-prenume["fr"] = [""];
+    fr: ["Chloé", "Juliette", "Caroline", "	Antoine", "Louis", "Matthieu", "Mélanie", "Fanny", "Cédric", "Angélique"]
+}
+
+prenume = {
+    ro: ["Ana", "Alexandru", "Mihaela", "Andreea", "Elena", "Adrian", "Andrei", "Alexandra", "Mihai", "Ionut", "Cristina", "Florin", "Daniel", "Cristina-Maria",
+    "Marian", "Marius", "Cristian", "Daniela", "Alina", "Maria", "Daniel-Georgian", "Marian-Andrei", "Marius-Andrei", "Ioana", "Constantin", "Nicoleta", 
+    "Georgiana", "Mariana", "Bogdan", "Vasile", "Gabriel", "Gabriela", "Nicolae", "Gheorghe", "George", "Ioan", "Valentin", "Adriana", "Ionela", "Catalin", 
+    "Stefan", "Ion", "Florentina", "Anca-Iulia", "Anamaria", "Simona", "Iulian", "Roxana", "Oana", "Irina", "Cristian-Vasile", "Daniela-Elena", "Florin-Andrei",
+    "Alina-Livia", "Maria-Elena", "Vasile-Ion", "Gabriel-Ionut", "Diana", "Mirela", "Iuliana", "Madalina", "Raluca", "Ionel", "Lucian", "Cosmin", "Sorin", 
+    "Loredana", "Claudia", "Monica", "Ramona-Maria", "George-Andrei", "Ana", "Ciprian", "Corina", "Laura", "Vlad Razvan", "Radu", "Liliana", "Valentina", 
+    "Viorel", "Iulia", "Claudia-Elena", "Monica-Larisa", "Ramona", "Ana-Ioana", "Ciprian-Andrei", "Corina-Maria", "Laura-Cristina", "Vlad-Alexandru", "Razvan-Andrei", 
+    "Radu-George", "Liliana-Elena", "Valentina-Andreea", "Viorel-Marin", "Iulia-Ioana", "Andrei-Marius", "Alexandra-Maria", "Mihai-Cristian", "Ionut-Marius"],
+
+    en: ["Mitchel", "Jennifer", "Mark", "George", "Hannah", "Linda", "Emma", "Andrew", "Kenneth", "Anthony", "William", "Susan"],
+
+    fr: ["Martin", "Thomas", "Laurent", "Dupont", "Roussel", "Sanchez", "Jean", "Leroux", "Vidal", "Lacroix", "Charles"]
+}
+
+function getNameOrigin(){
+    var getNmOrigin = document.getElementById("nameOrigin").value;
+    return getNmOrigin;
+}
+
+function getSurnameOrigin(){
+    var getSnOrigin = document.getElementById("nameOrigin").value;
+    return getSnOrigin;
+}
 
 function generateName() {
-    var fullName = nume[Math.floor(Math.random() * nume.length)] +" "+ prenume["en"][Math.floor(Math.random() * prenume.length)] // returneaza un undefined pe perenume ...
+    var nameOrigin = getNameOrigin();
+    var nameOriginArr = nume[nameOrigin];
+    var prenumeOrigin = getSurnameOrigin();
+    var prenumeOriginArr = prenume[prenumeOrigin];
+    var fullName = nameOriginArr[Math.floor(Math.random() * nameOriginArr.length)] +" "+ prenumeOriginArr[Math.floor(Math.random() * prenumeOriginArr.length)] // returneaza un undefined pe perenume ...
     document.getElementById("newName").value = fullName;
 }
 
 function copyFunction(newValue, copyValue) {
-    //nume["en"][Math...]
-    //nume.en[Math...]
-    //nume.fr[Math...]
     document.getElementById(newValue).select();
     document.execCommand("copy");
     var element = document.getElementById(copyValue);
